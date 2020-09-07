e61c4828a55782b2a6a6a29fb77ace7e42ddfee7
march-333 <70632538+march-333@users.noreply.github.com>
2020-09-07 21:59:11 +0800

    Create update.cpp

---
+++ update.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+bool isWin(char game[3][3]) {
+	bool win = false;
+	if (game[0][0] == game[0][1] && game[0][1] == game[0][2] && (game[0][0] == 'X' || game[0][0] == 'O')) win = true;
+	if (game[1][0] == game[1][1] && game[1][1] == game[1][2] && (game[1][0] == 'X' || game[1][0] == 'O')) win = true;
+	if (game[2][0] == game[2][1] && game[2][1] == game[2][2] && (game[2][0] == 'X' || game[2][0] == 'O')) win = true;
+	// column
+	if (game[0][0] == game[1][0] && game[1][0] == game[2][0] && (game[0][0] == 'X' || game[0][0] == 'O')) win = true;
+	if (game[0][1] == game[1][1] && game[1][1] == game[2][1] && (game[0][1] == 'X' || game[0][1] == 'O')) win = true;
+	if (game[0][2] == game[1][2] && game[1][2] == game[2][2] && (game[0][2] == 'X' || game[0][2] == 'O')) win = true;
+	// diagonal
+	if (game[0][0] == game[1][1] && game[1][1] == game[2][2] && (game[0][0] == 'X' || game[0][0] == 'O')) win = true;
+	if (game[0][2] == game[1][1] && game[1][1] == game[2][0] && (game[0][2] == 'X' || game[0][2] == 'O')) win = true;
+	return win;
+}
+
+int main(){
+	int i = 0;
+	int j=0;
+	char game[3][3] = { " " };
+	char player1 = 'X';
+	char player2 = 'O';
+	bool turn = true;
+	cout << "X = Player 1" << endl << "O = Player 2" << endl;
+	for (int n = 0; n < 9; n++) {
+		turn = !turn;
+		if (turn == false) {
+			cout << "Player 1 :";
+		}
+		else
+		{
+			cout << "Player 2 :";
+		}
+		cout<< "Which cell to mark? i:[0..2], j:[0..2]: ";
+		cin >> i >> j;
+		char inputnum = true;
+		while (inputnum) {
+			if (game[i][j] != 'X' && game[i][j] != 'O'&& i<=2 && j<=2) {
+				inputnum = false;
+				break;
+			}
+			cout << "Error!Input again" << endl;
+			cout << "Which cell to mark? i:[0..2], j:[0..2]: ";
+			cin >> i >> j;
+		}
+		if (turn == false)
+			game[i][j] = 'X';
+		else
+			game[i][j] = 'O';
+		if (isWin(game)) {
+			cout << "Win!" << endl;
+			break; // need to terminate the problem
+		}
+	}
+	if (!isWin(game)) // all celles with i=0 to 2 have been inputted above but no winner yet
+		cout << "Tie!" << endl;
+
+	// show the game to console
+	cout << game[0][0] << " " << game[0][1] << " " << game[0][2] << endl;
+	cout << game[1][0] << " " << game[1][1] << " " << game[1][2] << endl;
+	cout << game[2][0] << " " << game[2][1] << " " << game[2][2] << endl;
+	//return 1;
+
+}
